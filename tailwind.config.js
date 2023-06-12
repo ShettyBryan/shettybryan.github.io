c52836ef893226e54f8ec1235ec8d251aa033be7
Deathonater5 <shettyBryan@gmail.com>
2023-06-11 19:32:11 -0700

    New Tailwind Setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/css/*.css", "./public/js/*.js", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
