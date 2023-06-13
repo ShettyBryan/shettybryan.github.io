45b87173e848cbed8d4b5e7661135a19459b8a72
Deathonater5 <shettyBryan@gmail.com>
2023-06-12 17:01:47 -0700

    Able to make left half fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/css/*.css", "./public/js/*.js", "./index.html"],
+  content: ["./public/js/*.js", "./index.html"],
   theme: {
+    colors: {
+        'darkPurple': '#21112D',
+        'navyBlue': '#2B4E81',
+        'coolGreen': '#6AD1AE',
+        'lightNavyBlue': '#3E577B',
+        'pink': '#FF1CFB',
+        'limeGreen': '#82D8A1',
+        'lemonYellow': '#FFF6AD',
+        'white': '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
